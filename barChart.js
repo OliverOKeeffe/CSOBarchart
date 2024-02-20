e3844410d21f05742888fcd6e7640cbc6e431f9b
Oliver O'Keeffe <oliverokeeffe@Olivers-MacBook-Air.local>
2024-02-20 10:34:52 +0000

    Added to the code in attempt to get the data to load, still not loading

--- barChart.js
+++ barChart.js
@@ -1,32 +1,74 @@
-class BarChart {
-	constructor(obj) {
-		this.data = obj.data;
-		this.x = obj.x;
-		this.y = obj.y;
-		this.chartWidth = obj.chartWidth;
-		this.chartHeight = obj.chartHeight;
-		this.barWidth = obj.barWidth;
-		this.numBars = this.data.length;
-	}
+class BarChart{
+    constructor(obj){
+        this.data = obj.data;
+        this.yValue = obj.yValue;
+        this.chartWidth=obj.chartWidth;
+        this.chartHeight=obj.chartHeight;
+        this.xPos = obj.xPos;
+        this.yPos = obj.yPos;
+        this.axisLineColour = obj.axisLineColour;
+        this.labelTextSize = obj.labelTextSize;
+        this.labelPadding = obj.labelPadding;
+        this.labelColour = obj.labelColour;
+        this.labelRotation = obj.labelRotation;
+        this.barWidth = obj.barWidth;
+        this.barolour = obj.barColour;
+    }
 
-	render() {
-		translate(this.x, this.y);
-		noFill();
-		stroke(axisColour);
-		strokeWeight(axisThickness);
-		line(0, 0, this.chartWidth, 0);
-		line(0, 0, 0, -this.chartHeight);
+    render(){
+        push ();
+        translate (this.xPos,this.yPos);
+        stroke(this.axisLineColour)
+        line (0,0,0,-this.chartHeight);
+        line (0,0,this.chartWidth,0);
 
-		noStroke();
-		fill(barColour);
-		//prettier-ignore
-		let barGap = (this.chartWidth - (this.numBars * this.barWidth)) / (this.numBars + 1);
-		// console.log(barGap);
-		for (let i = 0; i < this.numBars; i++) {
-			//prettier-ignore
-			let jump = (barGap * (i+1)) + (this.barWidth * i);
-			let colHeight = this.data[i].Total * 10;
-			rect(jump, 0, this.barWidth, -colHeight);
-		}
-	}
+        let gap = (this.chartWidth - (this.data.length * this.barWidth)) / (this.data.length + 1)
+        let lables = this.data.map(d => d.Age_Group);
+        let scale = this.chartHeight/ max(this.data.map(d => d[this.yValue]));
+        console.log(scale)
+      
+        //This loop draws the horizontal elements, bars and lables
+        push()
+        translate(gap,0);
+        for(let i=0; i<this.data.length; i++){
+            //This draws the bars
+            fill ("#cf291d")
+            noStroke()
+            rect (0,0,this.barWidth, -this.data[i][this.yValue] * scale);
+
+            //This draws the lables
+            fill (this.labelColour)
+            noStroke()
+            textSize(this.lableTextSize)
+            textAlign(LEFT, CENTER)
+            push()
+            translate(this.barWidth/2,this.labelPadding)
+            rotate (this.labelRotation)
+
+            text(lables[i],0,0)
+            pop()
+
+            translate(gap+this.barWidth,0)
+          
+        
+        }
+        pop()
+
+        //This draws the vertical elements
+        let tickGap = this.chartHeight / 5;
+        let tickValue = max(this.data.map(d=> d[this.yValue]))/5
+        for(let i=0; i<=5; i++){
+            stroke("fff")              
+            line(0,-i*tickGap,-20,-i*tickGap);
+            fill ("#cf291d")
+            textSize(this.lableTextSize)
+            textAlign(RIGHT, CENTER)
+            text(tickValue*i,-20,-i*tickGap)
+            noStroke()
+        }
+
+        
+        
+        pop ();
+    }
 }
--- sketch.js
+++ sketch.js
@@ -1,44 +1,48 @@
+let barCharts = [];
 let data;
 let cleanData = [];
-let barCharts = [];
-
-let canvasWidth = 1600;
-let canvasHeight = 1600;
-
-// Colours
-let backgroundColour = "#e3e3e3";
-let axisColour = "#474747";
-let axisThickness = 3;
-let barColour = "#416096";
+let numRows;
 
 function preload() {
-	data = loadTable("data/CJA01.20240219T170251.csv", "csv", "header");
+  data = loadTable("data/Combined.csv", "csv", "header");
 }
-console.log(data)
 
 function setup() {
-	createCanvas(canvasWidth, canvasHeight);
+  background(50);
+  createCanvas(500, 500);
 
-	for (let i = 0; i < data.rows.length; i++) {
-		cleanData.push(data.rows[i].obj);
-	}
+  noLoop();
 
-	numBars = cleanData.length;
+  let numRows = data.rows.length;
+  for (let i = 0; i < numRows; i++) {
+    cleanData.push(data.rows[i].obj);
+  }
 
-	let barChart01 = {
-		data: cleanData,
-		chartWidth: 400,
-		chartHeight: 400,
-		x: 100,
-		y: 500,
-		barWidth: 30,
-	};
+  let barChart01 = {
+    data: cleanData,
+    yValue: "Total",
+    chartWidth: 200,
+    chartHeight: 200,
+    xPos: 100,
+    yPos: 350,
+    axisLineColour: "#d9d9d9",
+    barWidth: 10,
+    labelTextSize: 20,
+    labelPadding: 10,
+    labelColour: "#d17c4b",
+    labelRotation: 45,
+    barColour: "#cf291d",
+  };
 
-	barCharts.push(new BarChart(barChart01));
+  console.log()
+
+  barCharts.push(new BarChart(barChart01)); 
+
+  //barCharts.push(new BarChart(cleanData,80,80,50,350,"#ff0000"));
+  //barCharts.push(new BarChart(cleanData,400,400,50,450,"#d9d9d9"))
 }
-console.log(cleanData)
 
 function draw() {
-	background(backgroundColour);
-	barCharts.forEach((barChart01) => barChart01.render());
+  background(50);
+  barCharts.forEach((bar) => bar.render()); 
 }
